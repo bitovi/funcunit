2399adeaf6c63469e67fb7ccdc5e11fc54883a13
Brian Moschel <brian.moschel@gmail.com>
2010-04-16 03:32:39 -0500

    updating selenium server

--- funcunit.js
+++ funcunit.js
@@ -1,9 +1,15 @@
-steal.plugins('jquery/controller','jquery/controller/subscribe',
-			  'jquery/view/ejs',
-			  'jquery/model',
-			  'jquery/dom/fixtures',
-			  'jquery/dom/form_params')
-     .resources()
-     .models()
-     .controllers()
-     .views()
+load('steal/rhino/env.js');
+
+(function(){
+	Funcunit = function(){}
+	Funcunit.runTest = function(pageLoc){
+		Envjs(pageLoc, {
+			scriptTypes: {
+				"text/javascript": true,
+				"text/envjs": true
+			},
+			fireLoad: true,
+			logLevel: 2
+		});
+	}
+})();
