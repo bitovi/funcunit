08743a26cfd3f298ab283c19b8cbdb19167fb6f3
Brian Moschel <brian.moschel@gmail.com>
2010-04-25 01:22:45 -0500

    fixing funcunit with emails

--- funcunit.js
+++ funcunit.js
@@ -3,6 +3,8 @@ load('steal/rhino/env.js');
 (function(){
 	Funcunit = function(){}
 	Funcunit.runTest = function(pageLoc){
+		var path = new java.io.File(".").getCanonicalPath();
+		SeleniumDefaults.browserURL = "file:///"+path.replace("\\", "/")+"/"+pageLoc;
 		Envjs(pageLoc, {
 			scriptTypes: {
 				"text/javascript": true,
@@ -12,4 +14,21 @@ load('steal/rhino/env.js');
 			logLevel: 2
 		});
 	}
+	Funcunit.redirectOutputTo = function(path){
+		java.lang.System.setOut(new java.io.PrintStream(
+			new java.io.BufferedOutputStream(new java.io.FileOutputStream(path))));
+	}
+	Funcunit.emailFile = function(path){
+		if (typeof javax.mail.Session.getDefaultInstance != "function") {
+			print('usage: steal\\js -mail [arguments]')
+			quit()
+		}
+		load('steal/email/email.js');
+		var log = readFile(path);
+		print(log)
+		steal.Emailer.setup(EmailerDefaults);
+		print('here')
+		steal.Emailer.send(log)
+	}
+
 })();
