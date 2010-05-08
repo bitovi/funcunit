4157a7f20be307c08c8f9a119c91cb00042626bc
Brian Moschel <brian.moschel@gmail.com>
2010-05-08 14:49:42 -0500

    adding settings

---
+++ settings.js
@@ -0,0 +1,10 @@
+FuncUnit = {
+	// the domain where selenium will run
+    serverHost: "localhost",
+	// the port where selenium will run
+    serverPort: 4444,
+	// the domain/url where your page will run from (change if not filesystem)
+    //browserURL: "http://localhost/",
+	// the list of browsers that selenium runs tests on
+	browsers: ["*iexplore"]
+}
