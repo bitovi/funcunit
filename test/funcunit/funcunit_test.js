df1017a7e2d512471e52fb4820e5b98376ccfabe
Brian Moschel <brian.moschel@gmail.com>
2010-06-23 00:44:37 -0500

    restoring tests

--- test/funcunit/funcunit_test.js
+++ test/funcunit/funcunit_test.js
@@ -1,4 +1,15 @@
 module("funcunit test")
+
+test("Back to back opens", function(){
+	S.open("test/myotherapp.html", null, 10000);
+	
+	S.open("test/myapp.html", null, 10000);
+
+	S("#changelink").click().text(function(t){
+		equals(t, "Changed","href javascript run")
+	})
+})
+
 test("Copy Test", function(){
 
 	S.open("test/myapp.html", null, 10000);
@@ -30,4 +41,44 @@ test("drag test", function(){
 	
 	S.wait(2000)
 	S("#drag").dragTo({ x: "+100", y: "-100" })
+})
+
+test("iframe", function(){
+	
+	S("h2",0).text(function(text){
+		equals(text, "Goodbye World", "text of iframe")
+	})
+})
+test("waitHtml", function(){
+	S("#clickToChange").click().waitHtml(function(html){
+		return html == "changed"
+	}).html(function(html){
+		equals(html,"changed","wait actually waits")
+	})
+	
+})
+
+test("Next Test", function(){
+
+	S.open("test/myotherapp.html", null, 10000);
+	
+	S.wait(1000,function(){
+		ok('coolness')
+	})
+})
+
+test("URL Test", function(){
+	var path;
+	
+	path = FuncUnit.getAbsolutePath("http://foo.com")
+	equals(path, "http://foo.com", "paths match");
+	
+	FuncUnit.jmvcRoot = "http://localhost/"
+	path = FuncUnit.getAbsolutePath("//myapp/mypage.html")
+	equals(path, "http://localhost/myapp/mypage.html", "paths match");
+	
+	FuncUnit.jmvcRoot = null
+	path = FuncUnit.getAbsolutePath("//myapp/mypage.html")
+	equals(path, "../myapp/mypage.html", "paths match");
+	
 })
