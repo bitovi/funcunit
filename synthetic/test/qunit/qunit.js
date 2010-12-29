c6fb4948eaaa8d0c8127912efd6c34658ec6ffe1
Brian Moschel <brian.moschel@gmail.com>
2010-12-29 13:27:51 -0600

    making synthetic a submodule

--- synthetic/test/qunit/qunit.js
+++
@@ -1,8 +0,0 @@
-//we probably have to have this only describing where the tests are
-steal
- .plugins("jquery")
- .plugins("funcunit/synthetic")  //load your app
- .plugins('funcunit/qunit')  //load qunit
- .then("synthetic_test", "mouse_test", "key_test"
- 
- )
