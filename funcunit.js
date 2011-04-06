fa5d494e15ee64f05e20e82bbd7657d9a3efef57
Brian Moschel <brian.moschel@gmail.com>
2011-04-06 03:19:38 -0400

    accidentally commented out syn

--- funcunit.js
+++ funcunit.js
@@ -2,7 +2,7 @@
 steal.plugins('funcunit/qunit',
 	'funcunit/qunit/rhino')
 	.then('resources/jquery','resources/json','resources/selector')
-//	.plugins('funcunit/syn')
+	.plugins('funcunit/syn')
 //Now describe FuncUnit
 .then(function(){
 
