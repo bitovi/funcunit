c133d3f2610365958207b28b9157d788c09247b5
Brian Moschel <brian.moschel@gmail.com>
2011-11-12 01:43:19 -0500

    cooler google demo

--- test/google/google_test.js
+++ test/google/google_test.js
@@ -8,7 +8,15 @@ steal('funcunit', function(){
 	test('autocomplete', function(){
 		S('#lst-ib').type('Angry');
 		S('.gssb_e .gsq_a tr').size(4);
-		// S('.gssb_e .gsq_a tr:first').click();
-		S('#rso a:first').text("Angry Birds - Rovio")
+		S('#rso a:first').text("Angry Birds - Rovio", "Angry birds is the first link")
+		S('.vspii:first').click();
+		S('#nycp').visible("Preview shows up")
+	})
+	
+	test('image search', function(){
+		S('#lst-ib').type('Angry');
+		S('.gssb_e .gsq_a tr').size(4);
+		S(".mitem:contains('Images') a").click();
+		S('#rg_hi').visible("First image shows up")
 	})
 })
