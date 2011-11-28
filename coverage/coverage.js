5c2be375d49a8da50cabdf40e6a7593104b81f53
Brian Moschel <brian.moschel@gmail.com>
2011-11-27 22:51:00 -0500

    updates to coverage report

--- coverage/coverage.js
+++ coverage/coverage.js
@@ -1,5 +1,8 @@
 steal('funcunit/browser/resources/jquery.js', function(){
 	var data;
+	var pct = function(num){
+		return Math.round(num*1000)/10+"%";
+	}
 	$.ajax({
 		url: 'coverage.json',
 		dataType: 'json',
@@ -7,11 +10,13 @@ steal('funcunit/browser/resources/jquery.js', function(){
 			data = d;
 			var tr = [], 
 				stats;
+			$('.stats .line .stat').text(pct(d.total.lineCoverage))
+			$('.stats .block .stat').text(pct(d.total.blockCoverage))
 			for(var file in data.files){
 				tr.push("<tr>");
 				stats = data.files[file];
-				var linePercentage = Math.round(stats.lineCoverage*1000)/10+"%",
-					blockPercentage = Math.round(stats.blockCoverage*1000)/10+"%";
+				var linePercentage = pct(stats.lineCoverage),
+					blockPercentage = pct(stats.blockCoverage);
 				tr.push("<td>", "<a class='file' href='#'>", file, "</a>", "</td>");
 				tr.push("<td>", stats.lines, "</td>");
 				tr.push("<td>", "<img height='15' width='130' src='http://chart.apis.google.com/chart?chf=bg,s,676767&chxs=0,000000,0,0,_,676767|1,000000,0,0,_,676767&chxt=x,y&chbh=23,0,0&chs=130x15&cht=bhs&chco=585858,919191&chp=0,0.033&chma=2&chd=t:", parseInt(linePercentage, 10), "|100' />", linePercentage ,"</td>");
@@ -25,6 +30,7 @@ steal('funcunit/browser/resources/jquery.js', function(){
 	$('#coverage').delegate('.file', 'click', function(ev){
 		$("#closeButton").show().text('back to grid');
 		$('#coverage').hide();
+		$('.stats').hide();
 		
 		var fileName = $(ev.target).text(),
 			src = data.files[fileName].src,
@@ -47,6 +53,7 @@ steal('funcunit/browser/resources/jquery.js', function(){
 		$("#closeButton").hide();
 		$('#file').hide();
 		$('#coverage').show();
+		$('.stats').show();
 		$('.title').text("Code Coverage");
 	})
 })
