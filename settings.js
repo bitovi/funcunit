be908d0b25e432739a5bedde657722c93ab6c479
Brian Moschel <brian.moschel@gmail.com>
2012-01-10 11:50:21 -0500

    adding setting

--- settings.js
+++ settings.js
@@ -9,6 +9,9 @@ FuncUnit = {
 	// the number of milliseconds between commands, "slow" is 500 ms
 	// speed: "slow",
 	
+	// turn on if you want to exit hard with the -e flag
+	// failOnError: true,
+	
 	// any files or directories that coverage calculations should ignore
 	coverageIgnore: ['*/test', "*_test.js", "*jquery.js", "*qunit.js"]
 	
