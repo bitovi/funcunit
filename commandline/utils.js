d4abc8624dd80838842d524701768d9d6be86db0
Brian Moschel <brian.moschel@gmail.com>
2012-02-02 21:13:21 -0500

    adding support to specify settings in commandline parameter (-settings cookbook/settings.js)

--- commandline/utils.js
+++ commandline/utils.js
@@ -13,9 +13,14 @@ steal(function(){
 	 * 1. Manually before funcunit.js 
 	 * 2. FuncUnit.load will try to load settings.js if there hasn't been one loaded
 	 */
-	FuncUnit._loadSettingsFile = function(page){
-		var backupFunc = FuncUnit;
-		load('funcunit/settings.js')
+	FuncUnit._loadSettingsFile = function(page, settings){
+		var backupFunc = FuncUnit,
+			path = 'funcunit/settings.js';
+		if(settings){
+			path = settings;
+		}
+		load(path);
+		print('loading settings.js from '+path)
 		steal.extend(FuncUnit, backupFunc)
 		load('funcunit/commandline/output/output.js')
 	}
