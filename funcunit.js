60912e933f77bb24048ddccd4508fd1899d42e64
David Luecke <daff@neyeon.com>
2012-08-28 12:43:50 -0600

    Add triggerSyn in funcunit.js

--- funcunit.js
+++ funcunit.js
@@ -1,8 +1,17 @@
 //what we need from javascriptmvc or other places
-steal('jquery');
-steal('funcunit/syn', './browser/core.js', './browser/adapters/adapters.js',
+steal('jquery', 'funcunit/syn', './browser/core.js', './browser/adapters/adapters.js',
 	'./browser/open.js', './browser/actions.js', './browser/getters.js',
 	'./browser/traversers.js', './browser/queue.js', './browser/waits.js',
 function($, Syn, FuncUnit) {
+	// TODO this can be removed when steal.then works properly
+	if ( (window.FuncUnit && window.FuncUnit.jQuery) || window.jQuery ) {
+		((window.FuncUnit && window.FuncUnit.jQuery) || window.jQuery).fn.triggerSyn = function( type, options, callback ) {
+			if(!this[0]){
+				throw "Can't "+type.substring(1)+" because no element matching '"+this.selector+"' was found"
+			}
+			Syn(type, options, this[0], callback);
+			return this;
+		};
+	}
 	window.FuncUnit = window.S = FuncUnit;
 });
