9385d3da8368966e610452e36c8af52fbc8bcf1f
Brian Moschel <brian.moschel@gmail.com>
2012-11-08 19:59:18 -0500

    changing open to use join from root to calculate url

--- test/funcunit/open_test.js
+++ test/funcunit/open_test.js
@@ -10,18 +10,6 @@ test('S.open accepts a window', function() {
 	S("#seewhatyoutyped").text("typed javascriptmvc","typing");
 })
 
-test("URL Test", function(){
-	var path = FuncUnit.getAbsolutePath("http://foo.com")
-	equals(path, "/", "paths match");
-	
-	path = FuncUnit.getAbsolutePath("//myapp/mypage.html");
-	var root = steal.config().root;
-	if(!root.protocol){
-		root = steal.File(root.joinFrom(steal.pageUrl().dir(), true));
-	}
-	equals(path, root.join("myapp/mypage.html"), "paths match");
-})
-
 
 
 test("Back to back opens", function(){
