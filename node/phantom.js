f562384a6f17d6faba64a9e136fce9a4986610d3
Alexis Abril <alexis@bitovi.com>
2012-12-19 00:16:01 -0600

    Making node runner a bit more modular.

--- node/phantom.js
+++ node/phantom.js
@@ -1,23 +1,35 @@
-var phantom = require('node-phantom');
+require('../reporters/console.js');
+
+var phantom = require('node-phantom'),
+promise = require('promised-io/promise');
 
 exports.run = function(url) {
+	url += '?steal[browser]=phantomjs&steal[startFiles]=funcunit/node/client.js';
+
+	var deferred = new promise.Deferred();
 	phantom.create(function(err, ph) {
 		ph.createPage(function(err, page) {
 			page.onConsoleMessage = function(msg, line, file) {
-				if(msg && msg.indexOf('__EVT' === 0)) {
+				if(msg && msg.indexOf('__EVT') === 0) {
 					try {
 						var evt = JSON.parse(msg.substring(5));
 						if(evt.trigger) {
 							FuncUnit[evt.trigger](evt.data);
+
 							if(evt.trigger === 'done'){
+								deferred.resolve();
 								ph.exit();
 							}
 						}
-					} catch(e){}
+					} catch(e) {
+						deferred.reject(e);
+					}
 				}
 			};
 
 			page.open(url, function() {});
 		});
 	});
+
+	return deferred.promise;
 }
--- node/run.js
+++ node/run.js
@@ -1,8 +1,3 @@
-require('../reporters/console.js');
-
 var phantom = require('./phantom.js');
 
-var url = process.argv[2];
-url += '?steal[browser]=phantomjs&steal[startFiles]=funcunit/node/client.js';
-
-phantom.run(url);
+phantom.run(process.argv[2]);
