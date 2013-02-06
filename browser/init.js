fb33eefd9b8b0d4647acf74ddf24133283ee0cb4
Brian Moschel <brian.moschel@gmail.com>
2013-02-06 17:25:05 -0600

    jquery 1.9 compatibility

--- browser/init.js
+++ browser/init.js
@@ -1,4 +1,4 @@
-steal('jquery', function(jQuery) {
+steal('jquery', "funcunit/browser/lib/jquery-migrate-1.1.0.js", function(jQuery) {
 	var FuncUnit = window.FuncUnit || {};
 	// TODO: if FuncUnit needs its own jQuery, add a steal.config here to make that happen.
 	FuncUnit.jQuery = jQuery//.noConflict(true);
