95c3d705b0ca7e2fa0c94ca472edcef09bda11a1
Brian Moschel <brian.moschel@gmail.com>
2013-02-14 19:08:59 -0600

    removed jquery migrate

--- browser/init.js
+++ browser/init.js
@@ -1,5 +1,27 @@
-steal('jquery', "funcunit/browser/lib/jquery-migrate-1.1.0.js", function(jQuery) {
+steal('jquery', function(jQuery) {
 	var FuncUnit = window.FuncUnit || {};
+
+	jQuery.sub = function() {
+		function jQuerySub( selector, context ) {
+			return new jQuerySub.fn.init( selector, context );
+		}
+		jQuery.extend( true, jQuerySub, this );
+		jQuerySub.superclass = this;
+		jQuerySub.fn = jQuerySub.prototype = this();
+		jQuerySub.fn.constructor = jQuerySub;
+		jQuerySub.sub = this.sub;
+		jQuerySub.fn.init = function init( selector, context ) {
+			if ( context && context instanceof jQuery && !(context instanceof jQuerySub) ) {
+				context = jQuerySub( context );
+			}
+
+			return jQuery.fn.init.call( this, selector, context, rootjQuerySub );
+		};
+		jQuerySub.fn.init.prototype = jQuerySub.fn;
+		var rootjQuerySub = jQuerySub(document);
+		return jQuerySub;
+	};
+	
 	// TODO: if FuncUnit needs its own jQuery, add a steal.config here to make that happen.
 	FuncUnit.jQuery = jQuery//.noConflict(true);
 	return FuncUnit;
