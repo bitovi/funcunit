4456e6c9a62ef2743cd119c79686b36d18f391c4
Chasen Le Hara <chasen@chasenlehara.com>
2017-02-24 15:58:13 -0800

    3.2.0

---
+++ dist/amd/browser/adapters/adapters.js
@@ -0,0 +1,53 @@
+/*funcunit@3.1.0#browser/adapters/adapters*/
+define(function (require, exports, module) {
+    (function (global) {
+        var jasmineAdapter = require('./jasmine');
+        var jasmine2Adapter = require('./jasmine2');
+        var qunitAdapter = require('./qunit');
+        var mochaAdapter = require('./mocha');
+        var FuncUnit = require('../core');
+        var noop = function () {
+        };
+        var defaultAdapter = {
+            pauseTest: noop,
+            resumeTest: noop,
+            assertOK: noop,
+            equiv: function (expected, actual) {
+                return expected == actual;
+            }
+        };
+        FuncUnit.unit = defaultAdapter;
+        FuncUnit.attach = function (runner) {
+            var unit;
+            if (isQUnit(runner)) {
+                unit = qunitAdapter(runner);
+            } else if (isMocha(runner)) {
+                unit = mochaAdapter(runner);
+            } else if (isJasmine(runner)) {
+                unit = jasmineAdapter(runner);
+            } else if (isJasmine2(runner)) {
+                unit = jasmine2Adapter(runner);
+            } else {
+                unit = defaultAdapter;
+            }
+            FuncUnit.unit = unit;
+        };
+        function isQUnit(runner) {
+            return !!(runner.ok && runner.start && runner.stop);
+        }
+        function isMocha(runner) {
+            return !!(runner.setup && runner.globals && runner.reporter);
+        }
+        function isJasmine(runner) {
+            return !!(runner.getEnv && typeof window.waitsFor === 'function');
+        }
+        function isJasmine2(runner) {
+            return !!(runner.getEnv && typeof runner.clock === 'function' && !window.waitsFor);
+        }
+        FuncUnit.detach = function () {
+            FuncUnit.unit = defaultAdapter;
+        };
+    }(function () {
+        return this;
+    }()));
+});
