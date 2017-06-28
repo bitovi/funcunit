7b7a5935991f55dad560dbb48fd75c5da88f37c7
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-06-28 10:43:56 -0400

    3.4.1

---
+++ dist/amd/browser/adapters/qunit2.js
@@ -0,0 +1,21 @@
+/*funcunit@3.4.0#browser/adapters/qunit2*/
+define(function (require, exports, module) {
+    var FuncUnit = require('../core');
+    module.exports = function (QUnit) {
+        var done;
+        return {
+            pauseTest: function () {
+                done = FuncUnit.qunit2Assert.async();
+            },
+            resumeTest: function () {
+                done();
+            },
+            assertOK: function (assertion, message) {
+                FuncUnit.qunit2Assert.ok(assertion, message);
+            },
+            equiv: function (expected, actual) {
+                return QUnit.equiv(expected, actual);
+            }
+        };
+    };
+});
---
+++ dist/cjs/global.js
@@ -0,0 +1,5 @@
+/*global*/
+require('./funcunit.js');
+var FuncUnit = window.FuncUnit || {};
+window.jQuery = jQuery;
+module.exports = FuncUnit;
