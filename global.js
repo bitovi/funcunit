4681688d18bcec76000d437aa81ad0d90809448e
kdillon <kdillon@mathworks.com>
2019-03-28 16:42:53 -0400

    Removing the assignment of FuncUnit to the global window object. This has been causing a 'FuncUnit is not defined'. The reason why this was not caught in tests is because this occurs in the global.js object, executed as part of the script compilation phase

--- global.js
+++ global.js
@@ -1,6 +1,9 @@
 require("./funcunit");
 
 var FuncUnit = window.FuncUnit || {};
-window.jQuery = jQuery;
+
+// Removed to avoid stomping on product jQuery in case they're using a different version.
+// For details, see: https://github.com/bitovi/funcunit/issues/236
+//window.jQuery = jQuery; 
 
 module.exports = FuncUnit;
